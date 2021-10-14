181f6fc16cfea339f1d3a60934d26e0ad350db50
Dechon Ryan <dechon@tuta.io>
2021-10-14 11:56:03 -0700

    Add functions to format notes and add notes

--- tiny_apps/tiny_notes/notes.js
+++ tiny_apps/tiny_notes/notes.js
@@ -8,28 +8,52 @@ const generateNotes = () => {
 
     const notebook = document.createElement('div');
     notebook.id = 'notebook';
-    notebook.width = '500';
-    notebook.height = '500';
+    notebook.style.width = '500px';
+    notebook.style.height = '500px';
     notebook.style.border = `4px solid ${appList[0].color}`
     display.append(notebook)
 
+    const formatNote = note => {
+        let formattedNote = document.createElement('div')
+        formattedNote.id = note.id
+        formattedNote.innerHTML = note.text
+        formattedNote.className = 'note'
+    }
+
+    const addNote = text => {
+        const note = {
+            text: text,
+            id: Date.now()
+        }
+
+        notes.push(note.text)
+    }
+
     const form = document.createElement('form')
     const input = document.createElement('input')
+    input.type = 'text'
+    input.placeholder = 'Your note here...'
     const button = document.createElement('button')
+    button.type = 'submit'
+    button.innerHTML = 'Submit'
 
-    notes.forEach(note => {
-        const li = document.createElement('li')
-        const node = document.createTextNode(note)
-        li.appendChild(node)
-        allNotes.append(li)
+    form.append(input)
+    form.append(button)
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+
+        const text = input.value.trim(); // trim() removes trailing spaces
+        alert(text)
+        input.value = ''
     })
 
+    // notes.forEach(note => {
+    //     const li = document.createElement('li')
+    //     const node = document.createTextNode(note)
+    //     li.appendChild(node)
+    //     allNotes.append(li)
+    // })
 
-    const input = document.createElement('input')
-    input.type = 'text'
-    options.append(input)
-
-    const submit = document.createElement('button')
-    submit.type = 'submit'
-    options.append(form)
+    display.append(form)
 }
